d2f42dbc54f07865d685e27842cb5c839349537b
theanishtar <dangtt135@gmail.com>
2024-03-26 15:09:33 +0700

    feat: emit to client scope

--- resources/views/scope.blade.php
+++ resources/views/scope.blade.php
@@ -12,60 +12,12 @@
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <style>
-  .chat-row {
-    margin: 50px;
-  }
 
-
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-
-  ul li {
-    padding: 8px;
-    background: #928787;
-    margin-bottom: 20px;
-  }
-
-
-  ul li:nth-child(2n-2) {
-    background: #c3c5c5;
-  }
-
-
-  .chat-input {
-    border: 1px soild lightgray;
-    border-top-right-radius: 10px;
-    border-top-left-radius: 10px;
-    padding: 8px 10px;
-    color: #fff;
-  }
-  </style>
 </head>
 
 <body>
 
-  <div class="container">
-    <div class="row chat-row">
-      <div class="chat-content">
-        <ul>
 
-        </ul>
-      </div>
-
-      <div class="chat-section">
-        <div class="chat-box">
-          <div class="chat-input bg-primary" id="chatInput" contenteditable="">
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
     crossorigin="anonymous"></script>
@@ -80,20 +32,9 @@
     let socket_port = '3000';
     let socket = io(ip_address + ':' + socket_port);
 
-    let chatInput = $('#chatInput');
-
-    chatInput.keypress(function(e) {
-      let message = $(this).html();
-      console.log(message);
-      if (e.which === 13 && !e.shiftKey) {
-        socket.emit('sendChatToServer', message);
-        chatInput.html('');
-        return false;
-      }
-    });
-
     socket.on('sendChatToClient', (message) => {
       $('.chat-content ul').append(`<li>${message}</li>`);
+      alert(message)
     });
   });
   </script>
