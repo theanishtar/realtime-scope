3b13949b3826598a400556604b15927ac474f640
theanishtar <dangtt135@gmail.com>
2024-03-26 14:58:46 +0700

    feat: add nodejs server

--- resources/js/echo.js
+++
@@ -1,8 +0,0 @@
-import Echo from "laravel-echo"
-
-window.io = require('socket.io-client');
-
-window.Echo = new Echo({
-  broadcaster: 'socket.io',
-  host: window.location.hostname + ':6001' // Port 6001 là mặc định của Laravel Echo Server
-});
